df2f80b3a5d866afd273c59c5105f934cfd3c800
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-19 23:48:21 +0100

    Fix build issues

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,10 +1,15 @@
 import { FC } from 'react';
-import ServiceCard from '../components/ServiceCard';
 
 const Services: FC = () => {
   return (
     <div className="w-[640px] mx-auto flex flex-col gap-6 pt-[72px]">
-      <ServiceCard />
+      <div className="bg-white rounded-lg shadow border border-gray-100">
+        <div className="h-44 p-3 flex flex-col gap-5">
+          <h2 className="text-gray-600 text-lg font-medium font-poppins">
+            Usługi
+          </h2>
+        </div>
+      </div>
       <div className="w-[57.40px] h-[54.01px] bg-gray-50 rounded-md border" />
     </div>
   );
