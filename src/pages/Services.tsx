d1d982f22354cf3b8576e713c1267cb11b8917a8
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-19 23:45:50 +0100

    Add Services and Settings pages

---
+++ src/pages/Services.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import ServiceCard from '../components/ServiceCard';
+
+const Services: FC = () => {
+  return (
+    <div className="w-[640px] mx-auto flex flex-col gap-6 pt-[72px]">
+      <ServiceCard />
+      <div className="w-[57.40px] h-[54.01px] bg-gray-50 rounded-md border" />
+    </div>
+  );
+};
+
+export default Services; 
---
+++ src/pages/Settings.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const Settings: FC = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center pt-[72px]">
+      <h1 className="text-4xl font-bold text-primary">
+        Ustawienia
+      </h1>
+    </div>
+  );
+};
+
+export default Settings; 
