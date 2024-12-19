d9d09c2dde7776e54c8bcbac3e83b8255eebcc39
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-19 23:26:09 +0100

    Remove old pages and fix imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@ import { FC } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
-import Home from './pages/Home';
-import About from './pages/About';
+import Services from './pages/Services';
+import Settings from './pages/Settings';
 
 const App: FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+          <Route index element={<Services />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
     </Router>
