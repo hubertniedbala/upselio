55c1980f5f699b3f6278a45471b0a774d77715e8
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-20 00:55:41 +0100

    Hide TabNavigation when 'Wszystkie' tab is active

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { useDrawerStore } from '../store/drawerStore';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
@@ -7,6 +7,8 @@ import TabNavigation from '../components/TabNavigation';
 
 const MainLayout: FC = () => {
   const isOpen = useDrawerStore((state) => state.isOpen);
+  const location = useLocation();
+  const showTabNav = location.pathname !== '/all';
 
   return (
     <div className="min-h-screen bg-gray-50">
@@ -14,12 +16,14 @@ const MainLayout: FC = () => {
         <div className="h-[72px] bg-white shadow border-b border-gray-100">
           <Navbar />
         </div>
-        <div className="h-[112px] bg-white border-b border-gray-100">
-          <TabNavigation />
-        </div>
+        {showTabNav && (
+          <div className="h-[112px] bg-white border-b border-gray-100">
+            <TabNavigation />
+          </div>
+        )}
       </header>
 
-      <div className="pt-[184px]">
+      <div className={showTabNav ? "pt-[184px]" : "pt-[72px]"}>
         <main className={`transition-all duration-300 ${isOpen ? 'mr-[400px]' : ''}`}>
           <div className="container mx-auto px-8 py-6">
             <Outlet />
