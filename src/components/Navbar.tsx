a7d539cbe5113ccc3d333bf60ba45495fea430db
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-19 22:47:07 +0100

    Add Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,32 @@
- 
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar: FC = () => {
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="container mx-auto px-4">
+        <div className="flex h-16 items-center justify-between">
+          <Link to="/" className="text-xl font-bold text-primary-600">
+            Upselio
+          </Link>
+          <div className="space-x-4">
+            <Link 
+              to="/" 
+              className="text-gray-600 hover:text-primary-600 transition-colors"
+            >
+              Home
+            </Link>
+            <Link 
+              to="/about" 
+              className="text-gray-600 hover:text-primary-600 transition-colors"
+            >
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar; 
