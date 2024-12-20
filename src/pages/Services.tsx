23db48e0242c402dfbf66a886dd08a62ca3e2430
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-20 02:57:57 +0100

    Adjust ServiceCard vertical position

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -3,7 +3,7 @@ import ServiceCard from '../components/ServiceCard';
 
 const Services: FC = () => {
   return (
-    <div className="min-h-[calc(100vh-72px)] flex items-center justify-center">
+    <div className="h-full flex items-center justify-center -mt-20">
       <div className="w-[640px]">
         <ServiceCard />
       </div>
