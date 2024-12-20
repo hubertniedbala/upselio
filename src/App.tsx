102dc33112591908076f3122031a722fb842d17b
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-20 01:27:30 +0100

    Fix routing and layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import { FC } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import MainLayout from './layouts/MainLayout';
-import Services from './pages/Services.tsx';
-import Settings from './pages/Settings.tsx';
+import Services from './pages/Services';
+import Settings from './pages/Settings';
+import AllServices from './pages/AllServices';
 
 const App: FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<MainLayout />}>
+        <Route element={<MainLayout />}>
           <Route index element={<Services />} />
+          <Route path="all" element={<AllServices />} />
           <Route path="settings" element={<Settings />} />
         </Route>
       </Routes>
