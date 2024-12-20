8bc46fd0a552caa42663bf620e9e63394f10c8da
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-20 02:37:22 +0100

    Fix TypeScript error for PlusIcon component

--- src/components/ServiceCard.tsx
+++ src/components/ServiceCard.tsx
@@ -1,8 +1,20 @@
-import { FC } from 'react';
+import { FC, SVGProps } from 'react';
 import { ServiceIcon } from './icons';
 
-const PlusIcon: FC = () => (
-  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+interface IconProps extends SVGProps<SVGSVGElement> {
+  className?: string;
+}
+
+const PlusIcon: FC<IconProps> = ({ className, ...props }) => (
+  <svg 
+    width="20" 
+    height="20" 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    xmlns="http://www.w3.org/2000/svg"
+    className={className}
+    {...props}
+  >
     <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
   </svg>
 );
