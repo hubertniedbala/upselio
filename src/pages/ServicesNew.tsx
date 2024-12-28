0decc3e555c6e256ca6e48fae34f16e14c58cc19
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-28 23:51:05 +0100

    Fix pattern background scope: - Remove pattern from Tailwind config - Add pattern only to ServicesNew background - Set -z-10 for background layer - Add proper bg-gray-50 class - Add export statement

--- src/pages/ServicesNew.tsx
+++ src/pages/ServicesNew.tsx
@@ -1,9 +1,9 @@
 const ServicesNew = () => {
   return (
     <div className="relative min-h-screen">
-      {/* Background pattern tylko dla tej strony */}
+      {/* Background pattern TYLKO dla tła tej strony */}
       <div 
-        className="absolute inset-0 z-0"
+        className="absolute inset-0 -z-10 bg-gray-50"
         style={{
           backgroundImage: `url("data:image/svg+xml,<svg id='patternId' width='10' height='10' xmlns='http://www.w3.org/2000/svg'><defs><pattern id='a' patternUnits='userSpaceOnUse' width='10' height='10'><rect x='0' y='0' width='100%' height='100%' fill='%23F1F1F2'/><circle cx='2' cy='2' r='1' fill='%23D1D1D2'/></pattern></defs><rect width='800%' height='800%' fill='url(%23a)'/></svg>")`,
           backgroundRepeat: 'repeat',
@@ -11,10 +11,12 @@ const ServicesNew = () => {
         }}
       />
       
-      {/* Zawartość strony */}
+      {/* Zawartość strony - bez patternu */}
       <div className="relative z-10">
-        {/* ... reszta komponentów ... */}
+        {/* Tutaj komponenty bez patternu w tle */}
       </div>
     </div>
   );
-}; 
+};
+
+export default ServicesNew; 
