83bc281e1aaf7aeada032e5a0d2888dd1aac906f
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-29 01:07:53 +0100

    Update pattern SVG: - Simplify SVG code - Add opacity directly in SVG - Keep background color - Fix pattern visibility - Remove unnecessary groups

--- src/components/ServicePreview.tsx
+++ src/components/ServicePreview.tsx
@@ -1,6 +1,11 @@
 const ServicePreview = () => {
   return (
-    <div className="h-[calc(100vh-184px)] service-preview-bg relative">
+    <div 
+      className="h-[calc(100vh-184px)] relative"
+      style={{
+        background: `#F2F2F3 url("data:image/svg+xml,%3Csvg width='10' height='10' viewBox='0 0 10 10' xmlns='http://www.w3.org/2000/svg'%3E%3Ccircle cx='2' cy='2' r='1' fill='%23D1D1D2' fill-opacity='.4'/%3E%3C/svg%3E") repeat center !important`
+      }}
+    >
       <div className="h-full w-full overflow-y-auto custom-scrollbar">
         <div className="min-h-full flex items-center justify-center">
           <div className="w-[430px] my-6">
