95e0938530fa8793ba811b0d29332832faca4fb7
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-29 02:04:12 +0100

    Update all icons to use props: - Add IconProps interface support - Add className prop handling - Fix TypeScript errors - Keep consistent styling - Add proper exports

--- src/icons/interface/Plus.tsx
+++ src/icons/interface/Plus.tsx
@@ -1,7 +1,11 @@
-import { FC } from 'react';
+import { IconProps } from '../types';
 
-export const PlusIcon: FC<{ className?: string }> = ({ className }) => (
-  <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-  </svg>
-); 
+const PlusIcon = ({ className = "w-5 h-5" }: IconProps) => {
+  return (
+    <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+    </svg>
+  );
+};
+
+export default PlusIcon; 
