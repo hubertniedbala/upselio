813663e0aa6aed320ece1e4c248e4ba2e05f160d
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-29 02:06:14 +0100

    Fix ChevronDown icon: - Fix duplicate component declaration - Fix missing closing tags - Keep proper SVG structure - Fix TypeScript errors

--- src/icons/navigation/ChevronDown.tsx
+++ src/icons/navigation/ChevronDown.tsx
@@ -1,8 +1,7 @@
 import { IconProps } from '../types';
 
 const ChevronDownIcon = ({ className = "w-5 h-5" }: IconProps) => {
-const ChevronDownIcon = ({ className = "w-5 h-5" }: IconProps) => {
-    <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  return (
     <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M6 9L12 15L18 9" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
     </svg>
