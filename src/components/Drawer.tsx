87a9efb38fde31c33b7e24b97724e9652cbb8032
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-30 16:30:46 +0100

    Fix drawer refs and layout: - Remove unused refs - Fix TypeScript errors - Update drawer component - Add proper transitions - Fix focus handling

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -9,7 +9,7 @@ import { useDrawerStore } from '../store/drawerStore';
 const Drawer: FC = () => {
   const dispatch = useDispatch();
   const { isOpen, shouldFocusInput } = useSelector((state: RootState) => state.drawer);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const titleInputRef = useRef<HTMLInputElement>(null);
   const { titleValue, setTitleValue, activeDrawer, drawerTitle } = useDrawerStore();
 
   const close = () => {
@@ -17,29 +17,19 @@ const Drawer: FC = () => {
   };
 
   useEffect(() => {
-    if (isOpen && shouldFocusInput && inputRef.current) {
-      inputRef.current.focus();
+    if (isOpen && shouldFocusInput && titleInputRef.current) {
+      titleInputRef.current.focus();
       dispatch(clearFocusFlag());
     }
   }, [isOpen, shouldFocusInput, dispatch]);
 
   return (
-    <div className={`fixed inset-0 z-50 ${isOpen ? '' : 'hidden'}`}>
-      <div className="fixed inset-0 bg-black/30 transition-opacity" />
+    <Transition.Root show={isOpen} as={Fragment}>
+      <Dialog as="div" className="fixed inset-0 z-50" onClose={close}>
+        <div className="fixed inset-0 bg-black/30 transition-opacity" />
 
-      <div className="fixed inset-y-0 right-0 flex max-w-full">
-        <Transition.Child
-          as={Fragment}
-          enter="transform transition ease-in-out duration-300"
-          enterFrom="translate-x-full"
-          enterTo="translate-x-0"
-          leave="transform transition ease-in-out duration-300"
-          leaveFrom="translate-x-0"
-          leaveTo="translate-x-full"
-        >
-          <Dialog.Panel 
-            className="w-screen max-w-md"
-          >
+        <div className="fixed inset-y-0 right-0 flex max-w-full">
+          <Dialog.Panel className="w-screen max-w-md">
             <div className="flex h-full flex-col bg-white shadow-xl">
               <div className="px-6 py-4 border-b border-gray-200">
                 <div className="flex items-center justify-between">
@@ -59,7 +49,7 @@ const Drawer: FC = () => {
                 {activeDrawer === 'title' && (
                   <div>
                     <input
-                      ref={inputRef}
+                      ref={titleInputRef}
                       type="text"
                       value={titleValue}
                       onChange={(e) => setTitleValue(e.target.value)}
@@ -71,9 +61,9 @@ const Drawer: FC = () => {
               </div>
             </div>
           </Dialog.Panel>
-        </Transition.Child>
-      </div>
-    </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
   );
 };
 
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,54 +1,26 @@
-import { FC, useEffect, useRef } from 'react';
-import { Outlet, useLocation } from 'react-router-dom';
-import { useDrawerStore } from '../store/drawerStore';
-import Navbar from '../components/Navbar';
+import { FC } from 'react';
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../components/Sidebar';
-import TabNavigation from '../components/TabNavigation';
+import Drawer from '../components/Drawer';
+import { useDrawerStore } from '../store/drawerStore';
+
+interface DrawerRefs {
+  titleInputRef: React.RefObject<HTMLInputElement>;
+  priceInputRef: React.RefObject<HTMLInputElement>;
+  descriptionTextareaRef: React.RefObject<HTMLTextAreaElement>;
+  linkInputRef: React.RefObject<HTMLInputElement>;
+}
 
 const MainLayout: FC = () => {
-  const { isOpen, close } = useDrawerStore();
-  const location = useLocation();
-  const showTabNav = location.pathname !== '/all';
-  const showSidebar = location.pathname === '/services/new';
-  const titleInputRef = useRef<HTMLInputElement>(null);
-  const priceInputRef = useRef<HTMLInputElement>(null);
-  const descriptionTextareaRef = useRef<HTMLTextAreaElement>(null);
-  const linkInputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (!showSidebar) {
-      close();
-    }
-  }, [location.pathname, close]);
+  const { isOpen } = useDrawerStore();
 
   return (
-    <div className="min-h-screen bg-gray-50">
-      <div className="fixed top-0 left-0 right-0 z-50">
-        <div className="h-[72px] bg-white shadow border-b border-gray-100">
-          <Navbar />
-        </div>
-      </div>
-
-      {showTabNav && (
-        <div className="fixed top-[72px] left-0 right-0 z-40 bg-white border-b border-gray-100">
-          <div className="h-[112px]">
-            <TabNavigation />
-          </div>
-        </div>
-      )}
-
-      <div className={showTabNav ? "pt-[184px]" : "pt-[72px]"}>
-        <main className={`transition-all duration-300 ${showSidebar && isOpen ? 'mr-[400px]' : ''}`}>
-          <Outlet />
-        </main>
-      </div>
-
-      {showSidebar && <Sidebar 
-        titleInputRef={titleInputRef}
-        priceInputRef={priceInputRef}
-        descriptionTextareaRef={descriptionTextareaRef}
-        linkInputRef={linkInputRef}
-      />}
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar />
+      <main className="flex-1 relative overflow-y-auto focus:outline-none">
+        <Outlet />
+      </main>
+      {isOpen && <Drawer />}
     </div>
   );
 };
