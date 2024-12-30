2f9f804015bfd07e201fced60cb905529db5439f
hubertniedbala <hubert.niedbala@gmail.com>
2024-12-30 12:26:43 +0100

    Fix ServicesNew page: - Add ServicesNew component - Fix routing setup - Add Redux Provider - Add default service data - Fix page layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Services from './pages/Services';
 import ServicesNew from './pages/ServicesNew';
@@ -7,6 +7,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<MainLayout />}>
+        <Route index element={<Navigate to="/services" replace />} />
         <Route path="services" element={<Services />} />
         <Route path="services/new" element={<ServicesNew />} />
       </Route>
