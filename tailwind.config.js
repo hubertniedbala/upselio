2d4b766fd9d9c76770f386a165a4d40ccd7b63d1
hubertniedbala <hubert.niedbala@gmail.com>
2025-01-14 19:48:57 +0100

    Update styling configuration with new colors and utilities

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,22 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#0369a1'
+        primary: '#2271B1',
+        error: '#D63638',
+        secondary: '#3A81B9',
+        gray: {
+          50: '#F0F0F1',
+          100: '#EAECF0',
+          200: '#CFD4DC',
+          300: '#666C84',
+          400: '#475466',
+          500: '#344053',
+          600: '#0F1728',
+          900: '#111827'
+        }
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
       }
     },
   },
