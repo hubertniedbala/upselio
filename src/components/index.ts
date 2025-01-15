8087bc28b021af05527b6856b16cc70b55f21b02
hubertniedbala <hubert.niedbala@gmail.com>
2025-01-15 01:49:31 +0100

    Fix component imports and MainLayout

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as ServiceCard } from './ServiceCard';
+export { default as TabBar } from './TabBar';
+export { default as Drawer } from './Drawer';
+export { default as Header } from './Header'; 
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,28 +1,18 @@
-import { FC } from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
-import TabBar from '../components/TabBar';
-import Drawer from '../components/Drawer';
-import { useDrawerStore } from '../store/drawerStore';
+import { FC, ReactNode } from 'react';
+import { Header, TabBar } from '../components';
 
-const MainLayout: FC = () => {
-  const { isOpen } = useDrawerStore();
+interface MainLayoutProps {
+  children: ReactNode;
+}
 
+const MainLayout: FC<MainLayoutProps> = ({ children }) => {
   return (
     <div className="min-h-screen bg-gray-50">
-      {/* Górny pasek z logo i nawigacją */}
       <Header />
-
-      {/* Pasek z zakładkami i kontrolkami */}
       <TabBar />
-
-      {/* Główna zawartość */}
-      <div className="pt-[184px]">
-        <main className={`transition-all duration-300 ${isOpen ? 'mr-[400px]' : ''}`}>
-          <Outlet />
-        </main>
-        {isOpen && <Drawer />}
-      </div>
+      <main className="pt-[184px]">
+        {children}
+      </main>
     </div>
   );
 };
