9977b5ea76d19a10af3237a6fb072ba87dff949e
hubertniedbala <hubert.niedbala@gmail.com>
2025-01-15 20:20:09 +0100

    Add Tailwind configuration and Poppins font

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,12 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#2271B1',
-        error: '#D63638',
-        secondary: '#3A81B9',
-        gray: {
-          50: '#F0F0F1',
-          100: '#EAECF0',
-          200: '#CFD4DC',
-          300: '#666C84',
-          400: '#475466',
-          500: '#344053',
-          600: '#0F1728',
-          900: '#111827'
-        }
+        primary: {
+          DEFAULT: '#2271B1',
+          dark: '#1B5A8E'
+        },
+        secondary: '#4B5563',
+        error: '#D63638'
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif']
