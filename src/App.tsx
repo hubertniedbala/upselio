de7398c9bd8008b1590af141bbecbb62ac74e769
hubertniedbala <hubert.niedbala@gmail.com>
2025-01-15 14:02:51 +0100

    Update App component to use MainLayout correctly

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,21 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { FC } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import Services from './pages/Services';
 import ServicesNew from './pages/ServicesNew';
+import { Drawer } from './components';
 
-const App = () => {
-  console.log('App rendering'); // Debugging
-
+const App: FC = () => {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Navigate to="/services" replace />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/services/new" element={<ServicesNew />} />
-        <Route path="*" element={<Navigate to="/services" replace />} />
-      </Route>
-    </Routes>
+    <Router>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Services />} />
+          <Route path="/new" element={<ServicesNew />} />
+        </Routes>
+        <Drawer />
+      </MainLayout>
+    </Router>
   );
 };
 
